bfa92d3a2ad1f110bdbd5d5971a6f1428c12f2e6
Sem Hernandez <sem.tomas.homs@gmail.com>
2023-08-19 16:35:43 -0600

    [#11] Add the pagination in the bank list

--- credit/views.py
+++ credit/views.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import ListView
 from credit.models import Bank
@@ -6,13 +7,18 @@ from credit.models import Bank
 # Create your views here.
 def bank_list_view(request):
     list_bank_obj = Bank.objects.all()
+    paginator = Paginator(list_bank_obj, 10)
+    page_number = request.GET.get("page",1)
+    page_obj = paginator.get_page(page_number)
     context = {}
-    context['list_bank_obj'] = list_bank_obj
+    context['list_bank_obj'] = page_obj.object_list
+    context['page_obj'] = page_obj
     return render(request,'credit/bank_list.html', context)
 
 class BankListView(ListView):
     model = Bank
     context_object_name = 'list_bank_obj'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
